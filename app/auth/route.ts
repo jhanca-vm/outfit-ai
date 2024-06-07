79f42a2ab732e3c2eb47f190de9d3429dca5485d
Jhan Carlos Viloria <jhanca-vm@proton.me>
2024-06-07 13:54:14 -0500

    Update route.ts

--- app/auth/route.ts
+++ app/auth/route.ts
@@ -2,7 +2,11 @@ import { NextResponse } from 'next/server'
 import { createClient } from '../lib/supabase'
 
 export async function GET(request: Request) {
-  console.log(request)
+  console.log(
+    request.headers.get('host'),
+    request.headers.get('x-forwarded-host'),
+    request.headers.get('x-forwarded-for'),
+  )
   
   const url = new URL(request.url)
   const code = url.searchParams.get('code')
